ad76d272cd5ed474108266be9e13b920a5828cb3
D7703_26 <d7703_26@mac.local>
2018-05-31 15:47:18 +0900

    20180531

---
+++ DITdelivery/DetailViewController.swift
@@ -0,0 +1,80 @@
+//
+//  DetailViewController.swift
+//  UiTableViewController Test 02
+//
+//  Created by D7703_04 on 2018. 5. 28..
+//  Copyright © 2018년 lse. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var detailTableView: UITableView!
+    @IBOutlet weak var cellImageView: UIImageView!
+    
+    
+    var cellImage: String = ""
+    var local1: String = ""
+    var tel1: String = ""
+    var menu: String = ""
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //delegate connection
+        detailTableView.dataSource = self
+        detailTableView.delegate = self
+        
+        cellImageView.image = UIImage(named: cellImage)
+        // Do any additional setup after loading the view.
+    }
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "detailCell", for: indexPath)
+        
+//       if indexPath.row == 0 {
+//            cell.textLabel?.text = local1
+//            return cell
+//        } else if indexPath.row == 1 {
+//            cell.textLabel?.text = tel1
+//            return cell
+//        }else {
+//            cell.textLabel?.text = menu
+//            return cell
+//        }
+        
+        switch indexPath.row {
+        case 0:
+            cell.textLabel?.text = local1
+            return cell
+        case 1:
+            cell.textLabel?.text = tel1
+            return cell
+        default:
+            cell.textLabel?.text = menu
+            return cell
+        }
+    }
+    
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+}
